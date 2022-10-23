f9ce51f5a0fb269ffb73b637491a64ae63efc1e0
yr <172808H@mymail.nyp.edu.sg>
2022-10-24 01:42:22 +0800

    finish url unit test

--- url.py
+++ url.py
@@ -1,33 +1,107 @@
 from __future__ import annotations
 class Url:
 
-    def __init__(self, url: str):
+    def __init__(self, text: str):
+        if text == "":
+            raise Exception("Invalid URL - no scheme specified.")
 
-        # self.url = url
-        after = self.extract_scheme(url)
         self.setup()
-        self.domain_name = after
+        self.text = text
+
+        text = self.extract_scheme(text)
+        text = self.extract_fragment(text)
+        text = self.extract_query(text)
+        text = self.extract_path(text)
+        text = self.extract_userinfo(text)
+        text = self.extract_port(text)
         
+        print("FINAL TEXT", text)
+        if text != "":
+            self.domain_name = text
+
+        self.add_smarts()
+        
+    def setup(self):
+        self.user = None
+        self.password = None
+        self.domain_name = None
+        self.port = None
+        self.path = None
+        self.query = None
+        self.fragment = None
+
 
     def extract_scheme(self, url: str):
         # scheme = url.split(":")[0]
-        before, sep, after = url.partition("://")
+        before, sep, after = url.partition(":")
         self.scheme = before
-        if before == "http":
-            self.port = 80
-        else:
-            self.port = 443
         print(before)
         print(sep)
         print(after)
+        if before == url:
+            raise Exception("Invalid URL - no scheme specified.")
+        if after.startswith("//"):
+            after = after.replace("//", "")
         return after
+
+    def extract_fragment(self, url: str):
+        if "#" in url:
+            before, sep, after = url.partition("#")
+            self.fragment = after
+            return before
+        return url
     
-    def setup(self):
-        self.path = "/"
-        self.user = None
-        self.password = None
-        self.query = None
-        self.fragment = None
+    def extract_query(self, url: str):
+        if "?" in url:
+            before, sep, after = url.partition("?")
+            self.query = after
+            print("set query", after)
+            print("return before query", before)
+            return before
+        return url
+
+    def extract_path(self, url: str):
+        if "/" in url:
+            before, sep, after = url.partition("/")
+            self.path = "/" + after
+            return before
+        return url
+
+    def extract_userinfo(self, url: str):
+        if "@" in url:
+            before, sep, after = url.partition("@")
+            self.user = before.split(":")[0]
+            if ":" in before:
+                self.password = before.split(":")[1]
+            return after
+        return url
+
+    def extract_port(self, url: str):
+        # http://example.com:81
+        # http://me:secret@example.com#f1"
+        # ftp://user:password@example.com:987/path
+        # mailto:swlodkowski@smu.edu.sg
+        if ":" in url:
+            before, sep, after = url.partition(":")
+            self.port = int(after)
+            return before
+        return url
+    
+
+    def add_smarts(self):
+        print("add smart")
+        if self.port == None:
+            print("inside port 0")
+            if self.scheme == "http":
+                self.port = 80
+            elif self.scheme == "https":
+                self.port = 443
+        if self.path == None and self.scheme != "mailto" and self.scheme != "news":
+            self.path = "/"
+    
+    def __str__(self):
+        # "http://user:password@example.com:123/x/y/z?a=b#s"
+        return self.text
 
 if __name__ == "__main__":
     print("Not an executable program.")
--- url_test.py
+++ url_test.py
@@ -32,150 +32,151 @@ class UrlTest(unittest.TestCase):
             Url("http://example.com"),
             "http", "/", "example.com", port = 80
         )
+    #self, url, scheme, path, domain_name, query, fragment, port, user, password
+    """
+    The `Url` class should properly analyze complex addresses.
+    """
+    def test_http_extended(self):
+        self.assertEqualUrl(
+            Url("http://example.com/x?yy#zzz"),
+            "http", "/x", "example.com", "yy", "zzz", port = 80
+        )
 
-    # """
-    # The `Url` class should properly analyze complex addresses.
-    # """
-    # def test_http_extended(self):
-    #     self.assertEqualUrl(
-    #         Url("http://example.com/x?yy#zzz"),
-    #         "http", "/x", "example.com", "yy", "zzz", port = 80
-    #     )
+    """
+    The `Url` class should properly work with any scheme.
+    """
+    def test_https_scheme(self):
+        self.assertEqualUrl(
+            Url("https://example.com"),
+            "https", "/", "example.com", port = 443
+        )
 
-    # """
-    # The `Url` class should properly work with any scheme.
-    # """
-    # def test_https_scheme(self):
-    #     self.assertEqualUrl(
-    #         Url("https://example.com"),
-    #         "https", "/", "example.com", port = 443
-    #     )
+    """
+    The `Url` class should properly work with any port.
+    """
+    def test_http_port(self):
+        self.assertEqualUrl(
+            Url("http://example.com:81"),
+            "http", "/", "example.com", port = 81
+        )
 
-    # """
-    # The `Url` class should properly work with any port.
-    # """
-    # def test_http_port(self):
-    #     self.assertEqualUrl(
-    #         Url("http://example.com:81"),
-    #         "http", "/", "example.com", port = 81
-    #     )
+    """
+    The `Url` class should properly work with any port.
+    """
+    def test_http_path(self):
+        self.assertEqualUrl(
+            Url("http://example.com/test/"),
+            "http", "/test/", "example.com", port = 80
+        )
 
-    # """
-    # The `Url` class should properly work with any port.
-    # """
-    # def test_http_path(self):
-    #     self.assertEqualUrl(
-    #         Url("http://example.com/test/"),
-    #         "http", "/test/", "example.com", port = 80
-    #     )
+    """
+    The `Url` class should properly work with paths using any filename.
+    """
+    def test_http_filename(self):
+        self.assertEqualUrl(
+            Url("http://example.com/test/filename"),
+            "http", "/test/filename", "example.com", port = 80
+        )
 
-    # """
-    # The `Url` class should properly work with paths using any filename.
-    # """
-    # def test_http_filename(self):
-    #     self.assertEqualUrl(
-    #         Url("http://example.com/test/filename"),
-    #         "http", "/test/filename", "example.com", port = 80
-    #     )
+    """
+    The `Url` class should properly work with query strings.
+    """
+    def test_http_query(self):
+        self.assertEqualUrl(
+            Url("http://example.com?a=b"),
+            "http", "/", "example.com", "a=b", port = 80
+        )
 
-    # """
-    # The `Url` class should properly work with query strings.
-    # """
-    # def test_http_query(self):
-    #     self.assertEqualUrl(
-    #         Url("http://example.com?a=b"),
-    #         "http", "/", "example.com", "a=b", port = 80
-    #     )
+    """
+    The `Url` class should properly work with fragments.
+    """
+    def test_http_fragment(self):
+        self.assertEqualUrl(
+            Url("http://example.com#f1"),
+            "http", "/", "example.com", fragment = "f1", port = 80
+        )
 
-    # """
-    # The `Url` class should properly work with fragments.
-    # """
-    # def test_http_fragment(self):
-    #     self.assertEqualUrl(
-    #         Url("http://example.com#f1"),
-    #         "http", "/", "example.com", fragment = "f1", port = 80
-    #     )
+    """
+    The `Url` class should properly work with user and password information.
+    """
+    def test_http_userinfo(self):
+        self.assertEqualUrl(
+            Url("http://me:secret@example.com#f1"),
+            "http", "/", "example.com", fragment = "f1", port = 80, user = "me", password = "secret"
+        )
 
-    # """
-    # The `Url` class should properly work with user and password information.
-    # """
-    # def test_http_userinfo(self):
-    #     self.assertEqualUrl(
-    #         Url("http://me:secret@example.com#f1"),
-    #         "http", "/", "example.com", fragment = "f1", port = 80, user = "me", password = "secret"
-    #     )
+    """
+    The `Url` class should properly work with `file` protocol urls.
+    """
+    def test_file_protocol(self):
+        self.assertEqualUrl(
+            Url("file:///C:/Users/UserName/index.html"),
+            "file", "/C:/Users/UserName/index.html"
+        )
 
-    # """
-    # The `Url` class should properly work with `file` protocol urls.
-    # """
-    # def test_file_protocol(self):
-    #     self.assertEqualUrl(
-    #         Url("file:///C:/Users/UserName/index.html"),
-    #         "file", "/C:/Users/UserName/index.html"
-    #     )
+    """
+    The `Url` class should properly work with `mail` protocol urls.
+    """
+    def test_mail_protocol(self):
+        self.assertEqualUrl(
+            Url("mailto:swlodkowski@smu.edu.sg"),
+            "mailto", domain_name = "smu.edu.sg", user = "swlodkowski"
+        )
 
-    # """
-    # The `Url` class should properly work with `mail` protocol urls.
-    # """
-    # def test_mail_protocol(self):
-    #     self.assertEqualUrl(
-    #         Url("mailto:swlodkowski@smu.edu.sg"),
-    #         "mailto", domain_name = "smu.edu.sg", user = "swlodkowski"
-    #     )
+    """
+    The `Url` class should properly work with `mail` protocol urls with a query.
+    """
+    def test_mail_protocol_query(self):
+        self.assertEqualUrl(
+            Url("mailto:swlodkowski@smu.edu.sg?subject=SE101"),
+            "mailto", domain_name = "smu.edu.sg", query = "subject=SE101", user = "swlodkowski"
+        )
 
-    # """
-    # The `Url` class should properly work with `mail` protocol urls with a query.
-    # """
-    # def test_mail_protocol_query(self):
-    #     self.assertEqualUrl(
-    #         Url("mailto:swlodkowski@smu.edu.sg?subject=SE101"),
-    #         "mailto", domain_name = "smu.edu.sg", query = "subject=SE101", user = "swlodkowski"
-    #     )
+    """
+    The `Url` class should properly work with `usenet` protocol urls.
+    """
+    def test_usenet_protocol(self):
+        self.assertEqualUrl(
+            Url("news:example"),
+            "news", None, "example"
+        )
 
-    # """
-    # The `Url` class should properly work with `usenet` protocol urls.
-    # """
-    # def test_usenet_protocol(self):
-    #     self.assertEqualUrl(
-    #         Url("news:example"),
-    #         "news", None, "example"
-    #     )
+    """
+    The `Url` class should properly work with `ftp` protocol urls.
+    """
+    def test_ftp_protocol(self):
+        self.assertEqualUrl(
+            Url("ftp://user:password@example.com:987/path"),
+            "ftp", "/path", domain_name = "example.com", user = "user", password = "password", port = 987
+        )
 
-    # """
-    # The `Url` class should properly work with `ftp` protocol urls.
-    # """
-    # def test_ftp_protocol(self):
-    #     self.assertEqualUrl(
-    #         Url("ftp://user:password@example.com:987/path"),
-    #         "ftp", "/path", domain_name = "example.com", user = "user", password = "password", port = 987
-    #     )
+    """
+    The `Url` class should properly serialize back to a valid address.
+    """
+    def test_to_string(self):
+        address = "http://user:password@example.com:123/x/y/z?a=b#s"
+        url = Url(address)
+        # self.assertEqualUrl(url, "http", "/x/y/z", "example.com", "a=b#s", "s", 123, "user", "password") #error fix 1, query not suppose to have #
+        self.assertEqualUrl(url, "http", "/x/y/z", "example.com", "a=b", "s", 123, "user", "password")
+        self.assertEqual(
+            str(url), address
+        )
 
-    # """
-    # The `Url` class should properly serialize back to a valid address.
-    # """
-    # def test_to_string(self):
-    #     address = "http://user:password@example.com:123/x/y/z?a=b#s"
-    #     url = Url(address)
-    #     self.assertEqualUrl(url, "http", "/x/y/z", "example.com", "a=b#s", "s", 123, "user", "password")
-    #     self.assertEqual(
-    #         str(url), address
-    #     )
+    """
+    The `Url` class should not serialize an address with invalid slashes.
+    """
+    def test_to_string_missing_slash(self):
+        with self.assertRaises(Exception) as context:
+            url = Url("invalid_url")
+        self.assertEqual(str(context.exception), "Invalid URL - no scheme specified.")
 
-    # """
-    # The `Url` class should not serialize an address with invalid slashes.
-    # """
-    # def test_to_string_missing_slash(self):
-    #     with self.assertRaises(Exception) as context:
-    #         url = Url("invalid_url")
-    #     self.assertEqual(str(context.exception), "Invalid URL - no scheme specified.")
-
-    # """
-    # The `Url` class should not serialize an empty address.
-    # """
-    # def test_empty(self):
-    #     with self.assertRaises(Exception) as context:
-    #         url = Url("")
-    #     self.assertEqual(str(context.exception), "Invalid URL - no scheme specified.")
+    """
+    The `Url` class should not serialize an empty address.
+    """
+    def test_empty(self):
+        with self.assertRaises(Exception) as context:
+            url = Url("")
+        self.assertEqual(str(context.exception), "Invalid URL - no scheme specified.")
 
 if __name__ == "__main__":
     unittest.main()
